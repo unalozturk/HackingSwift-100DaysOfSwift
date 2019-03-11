b8676bf177601574d65af946324b925a66045622
Ünal Öztürk <ozturkunal@outlook.com>
2019-03-12 01:20:58 +0300

    ....

---
+++ Day33/Project7/Project7/Petitions.swift
@@ -0,0 +1,15 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Ünal Öztürk on 8.03.2019.
+//  Copyright © 2019 Ünal Öztürk. All rights reserved.
+//
+
+import Foundation
+
+
+struct Petitions: Codable {
+    var results: [Petition]
+    
+}
